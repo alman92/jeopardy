b11857726936d181a330ac5ca7b942df5a9e29cb
Alex Manno <alman92@gmail.com>
2016-09-19 12:31:55 -0400

    Cool CSS Animation added

--- script.js
+++ script.js
@@ -1,10 +1,90 @@
-var categories = ["World Geography and Harambe", "2", "3", "4", "5", "6"]
+var categories = ["World Geography", "Mythology", "Modern American Politics", "DC Comics", "Food", "Media"]
+var values = ["$200", "$400", "$600", "$800", "$1000"]
 
 for(var i = 0; i < categories.length; i++){
   $(".categories").append("<div class='catTitle'>"+categories[i]+"</div>")
-  $(".question-container").append("<div class='question-row' id='"+categories[i]+"'></div>")
+  $(".question-container").append("<div class='question-row' id='"+i+"'></div>")
+  populateNumbers(i)
 }
 
-var values = ["$200", "$400", "$600", "$800", "$1000"]
+function populateNumbers(column){
+  console.log(column)
+  for(var i = 0; i < values.length; i++){
+    $("#" + column).append("<div class='question'>"+ values[i]+"</div>")
+  }
+}
 
-// for(var i = 0; i <
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var colors = new Array(
+  [62,35,255],
+  [60,255,60],
+  [255,35,98],
+  [45,175,230],
+  [255,0,255],
+  [255,128,0]);
+
+var step = 0;
+//color table indices for:
+// current color left
+// next color left
+// current color right
+// next color right
+var colorIndices = [0,1,2,3];
+
+//transition speed
+var gradientSpeed = 0.002;
+
+function updateGradient()
+{
+
+  if ( $===undefined ) return;
+
+var c0_0 = colors[colorIndices[0]];
+var c0_1 = colors[colorIndices[1]];
+var c1_0 = colors[colorIndices[2]];
+var c1_1 = colors[colorIndices[3]];
+
+var istep = 1 - step;
+var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+var color1 = "rgb("+r1+","+g1+","+b1+")";
+
+var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+var color2 = "rgb("+r2+","+g2+","+b2+")";
+
+ $('#gradient').css({
+   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+
+  step += gradientSpeed;
+  if ( step >= 1 )
+  {
+    step %= 1;
+    colorIndices[0] = colorIndices[1];
+    colorIndices[2] = colorIndices[3];
+
+    //pick two new target color indices
+    //do not pick the same as the current one
+    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+
+  }
+}
+
+setInterval(updateGradient,10);
